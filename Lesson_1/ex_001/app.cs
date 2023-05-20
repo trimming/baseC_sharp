de8eaba2522d9537353b8e497d7b8df972d4000d
trimming <lexgolikoff@gmail.com>
2023-05-20 12:38:55 +0300

    some update to app.cs

--- Lesson_1/ex_001/app.cs
+++ Lesson_1/ex_001/app.cs
@@ -1,2 +1,18 @@
 console.clear();
-console.write('Введите два числа: ')
+console.write('Введите первое число для сравнения: ');
+int num1 = int.Parse(console.ReadLine());
+console.write('Введите второе число для сравнения: ');
+int num2 = int.Parse(console.ReadLine());
+while( num1 == num2 ){
+    Console.WriteLine('Вы ввели два одинаковых числа!');
+    console.write('Введите второе число для сравнения: ');
+    num2 = int.Parse(console.ReadLine());
+}
+if(num1 > num2) 
+{
+    Console.WriteLine($'max = {num1}');
+}
+else
+{
+    Console.WriteLine($'max = {num2}');
+}
