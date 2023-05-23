d846c22137af450035a9e5db3c68c7392450b914
Alexey Golikov <lexgolikoff@gmail.com>
2023-05-24 00:37:22 +0300

    some update to Lesson_1

---
+++ Lesson_1/ex_003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if(num % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("не четное");
+}
---
+++ Lesson_2/ex_002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+int num = new Random().Next();
+Console.WriteLine(num);
+if (num < 100 && num > -100) {
+    Console.WriteLine("третьей цифры нет");    
+} else {
+    int num1 = num;
+    int count = 0;
+    while(num1 > 999 || num1 < -999) {
+        num1 = num1 / 10;
+        count++;
+    }
+    Console.WriteLine(Math.Abs(num1) % 10);    
+}
