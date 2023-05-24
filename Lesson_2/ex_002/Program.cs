310d73cb26c01b1330b4882df2e44a95776e26d8
Alexey Golikov <lexgolikoff@gmail.com>
2023-05-24 18:12:06 +0300

    some update to ex_002

--- Lesson_2/ex_002/Program.cs
+++ Lesson_2/ex_002/Program.cs
@@ -4,15 +4,16 @@
 // 32679 -> 6
 Console.Clear();
 int num = new Random().Next();
+
 Console.WriteLine(num);
 if (num < 100 && num > -100) {
     Console.WriteLine("третьей цифры нет");    
 } else {
     int num1 = num;
-    int count = 0;
+    
     while(num1 > 999 || num1 < -999) {
         num1 = num1 / 10;
-        count++;
+        
     }
     Console.WriteLine(Math.Abs(num1) % 10);    
 }
