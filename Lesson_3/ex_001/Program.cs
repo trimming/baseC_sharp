5b0bcbaeaa0d927359af979bdb4aeedbeb813023
Alexey Golikov <lexgolikoff@gmail.com>
2023-05-28 12:45:04 +0300

    created dir ex_001 for nineteenth task

---
+++ Lesson_3/ex_001/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите число: ");
+string numberToTest = Console.ReadLine();
+Console.WriteLine(numberToTest.Length / 2);
+int test = 0; 
+for(int i = 0; i < (numberToTest.Length / 2); i++) {
+    if(numberToTest[i] == numberToTest[(numberToTest.Length - 1)- i]) {
+        test++;
+    }    
+}
+
+if(test == numberToTest.Length / 2) {
+    Console.Write($"{numberToTest} - число палиндром");
+} else {
+    Console.Write($"{numberToTest} - обычное число");
+}
