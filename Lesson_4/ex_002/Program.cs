8c5bfe9ddd9c84642d92bd21d44e06ce7802e158
Alexey Golikov <lexgolikoff@gmail.com>
2023-06-01 20:58:44 +0300

    created dir ex_002 for twenty seventh task

---
+++ Lesson_4/ex_002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int getSumDigitOfNum(int num) {
+    int sum = 0;
+    while(num > 0) {
+        int modulo = num % 10;
+        sum = sum + modulo;
+        num /= 10; 
+    }
+    return sum;
+}
+Console.Clear();
+int number = new Random().Next();
+Console.WriteLine($"Сумма цифр в числе {number} равна {getSumDigitOfNum(number)}");
+
