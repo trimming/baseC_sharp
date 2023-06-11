8daf145b94fe6f1bee58e706f3e94a4cbb170291
Alexey Golikov <lexgolikoff@gmail.com>
2023-06-11 11:03:22 +0300

    create dir for Task#47

---
+++ Lesson_7/Task#47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+using System;
+using static System.Console;
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble();
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]:f2} ");
+        }
+        WriteLine();
+    }
+}
+Clear();
+PrintArray(GetArray(3, 4));
