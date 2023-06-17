d1cabf7b035395105c315784568ca09358eab712
Alexey Golikov <lexgolikoff@gmail.com>
2023-06-17 20:54:07 +0300

    create dir Task#58

---
+++ Lesson_8/Task#58/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+using System;
+using static System.Console;
+Clear();
+int[,] matrix1 = GetArray(2, 2);
+PrintArray(matrix1);
+WriteLine();
+int[,] matrix2 = GetArray(2, 2);
+PrintArray(matrix2);
+WriteLine();
+PrintArray(MultiMatrix(matrix1, matrix2));
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+int[,] MultiMatrix(int[,] array, int[,] array1)
+{
+    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < result.GetLength(1); k++)
+            {
+                result[i, j] += array[i, k] * array1[k, j];
+            }
+        }
+    }
+    return result;
+}
