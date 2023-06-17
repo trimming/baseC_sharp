cfea1c0ac2f6accd3a7585447c01a27b84fcf472
Alexey Golikov <lexgolikoff@gmail.com>
2023-06-17 21:51:35 +0300

    create dir Task#60

---
+++ Lesson_8/Task#60/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+using System;
+using static System.Console;
+Clear();
+int[,,] collection = GetArray(2, 2, 2);
+PrintArray(collection);
+WriteLine();
+
+int[,,] GetArray(int m, int n, int l)
+{
+    int[,,] result = new int[m, n, l];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < l; k++)
+            {
+                result[i, j, k] = new Random().Next(1, 10);
+            }
+        }
+    }
+    return result;
+}
+void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(1); k++)
+            {
+                Write($"{inArray[i, j, k],4}");
+            }
+            WriteLine();
+        }        
+    }
+}
